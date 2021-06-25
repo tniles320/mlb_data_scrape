a524f207b63c8e4abbb7a888847315624a757d1a
Taylor Niles <tniles320@gmail.com>
2021-06-25 11:55:21 -0700

    connected server to local mysql database, added sql query to add and update team data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const rp = require("request-promise-native");
+const mysql = require("mysql");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  // input password to connect to local database
+  password: "password",
+  database: "mlb_standingsdb",
+});
 
 async function main() {
   console.log("Making API request...");
@@ -26,10 +36,9 @@ async function main() {
     json: true,
   });
 
-  const resultsArr = results.records;
   const teamData = [];
 
-  resultsArr.forEach((result) => {
+  results.records.forEach((result) => {
     if (result.standingsType === "regularSeason") {
       result.teamRecords.forEach((team) => {
         teamData.push({
@@ -51,6 +60,24 @@ async function main() {
     //     res.json(teamData);
     //   });
     // }
+  });
+  teamData.forEach((team) => {
+    connection.query(
+      `INSERT INTO teams(team_id, team_name, win_streak, wins, losses, win_percentage, games_played) VALUES(?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE win_streak=${team.winStreak}, wins=${team.wins}, losses=${team.losses}, win_percentage=${team.winPercentage}, games_played=${team.gamesPlayed}`,
+      [
+        team.teamId,
+        team.team,
+        team.winStreak,
+        team.wins,
+        team.losses,
+        team.winPercentage,
+        team.gamesPlayed,
+      ],
+      (err, res) => {
+        if (err) throw err;
+        console.log("Team data added");
+      }
+    );
   });
 
   app.get("/", function (req, res) {
