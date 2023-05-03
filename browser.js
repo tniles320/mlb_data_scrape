ec057adffff1b51234b1e6e3769d90c874516723
Taylor <tniles320@gmail.com>
2023-05-02 20:41:31 -0700

    updated headless mode, dependencies, and commented out db entry

--- browser.js
+++ browser.js
@@ -2,21 +2,23 @@ const puppeteer = require("puppeteer");
 const apiObj = require("./apiCall");
 
 async function startBrowser() {
-  let browser;
-  try {
-    console.log("Opening the browser...");
-    browser = await puppeteer.launch({
-      headless: true,
-      args: ["--disable-setuid-sandbox"],
-      ignoreHTTPSErrors: true,
-    });
-  } catch (err) {
-    console.log("Could not create a browser instance => : ", err);
-    apiObj.apiCall();
-  }
-  return browser;
+	let browser;
+	try {
+		console.log("Opening the browser...");
+		browser = await puppeteer.launch({
+			// headless: true, // (default) enables old Headless
+			headless: "new", // enables new Headless
+			// headless: false, // enables “headful” mode
+			args: ["--disable-setuid-sandbox"],
+			ignoreHTTPSErrors: true,
+		});
+	} catch (err) {
+		console.log("Could not create a browser instance => : ", err);
+		apiObj.apiCall();
+	}
+	return browser;
 }
 
 module.exports = {
-  startBrowser,
+	startBrowser,
 };
--- pageScraper.js
+++ pageScraper.js
@@ -2,58 +2,54 @@ const fs = require("fs");
 const functions = require("./dbEntry");
 
 const scraperObject = {
-  url: "https://www.mlb.com/standings",
-  async scraper(browser) {
-    let page = await browser.newPage();
+	url: "https://www.mlb.com/standings",
+	async scraper(browser) {
+		let page = await browser.newPage();
 
-    console.log(`Navigate to ${this.url}...`);
+		console.log(`Navigating to ${this.url}.`);
 
-    // Navigate to web page
-    await page.goto(this.url);
+		// Navigate to web page
+		await page.goto(this.url);
 
-    // Wait for the required DOM to be rendered
-    await page.waitForSelector("main");
+		// Wait for the required DOM to be rendered
+		await page.waitForSelector("main");
 
-    let data = await page.$$eval(
-      ".responsive-datatable__scrollable > div > table > tbody > tr",
-      (tableData) => {
-        teamData = [];
+		console.log("Page loaded, collecting data.");
 
-        tableData.map((data) => {
-          teamData.push({
-            teamId: parseInt(
-              data
-                .querySelector(".p-logo")
-                .getAttribute("src")
-                .match(/\d+/gi)[0]
-            ),
-            team: data
-              .querySelector(".team--name > a")
-              .getAttribute("data-team-name"),
-            winStreak:
-              '"' + data.querySelector(".col-7 > span").textContent + '"',
-            losses: data.querySelector(".col-2 > span").textContent,
-            wins: data.querySelector(".col-1 > span").textContent,
-            winPercentage: data.querySelector(".col-3 > span").textContent,
-            gamesPlayed:
-              parseInt(data.querySelector(".col-1 > span").textContent) +
-              parseInt(data.querySelector(".col-2 > span").textContent),
-          });
-        });
-        return teamData;
-      }
-    );
+		let data = await page.$$eval(
+			".responsive-datatable__scrollable > div > table > tbody > tr",
+			(tableData) => {
+				teamData = [];
 
-    fs.writeFile("teams.json", JSON.stringify(data, null, 2), function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(
-        "The data has been scraped and saved successfully! View it at './teams.json'"
-      );
-    });
-    functions.dbEntry(data);
-  },
+				tableData.map((data) => {
+					teamData.push({
+						teamId: parseInt(
+							data.querySelector(".p-logo").getAttribute("src").match(/\d+/gi)[0]
+						),
+						team: data.querySelector(".team--name > a").getAttribute("data-team-name"),
+						winStreak: '"' + data.querySelector(".col-7 > span").textContent + '"',
+						losses: data.querySelector(".col-2 > span").textContent,
+						wins: data.querySelector(".col-1 > span").textContent,
+						winPercentage: data.querySelector(".col-3 > span").textContent,
+						gamesPlayed:
+							parseInt(data.querySelector(".col-1 > span").textContent) +
+							parseInt(data.querySelector(".col-2 > span").textContent),
+					});
+				});
+				return teamData;
+			}
+		);
+
+		fs.writeFile("teams.json", JSON.stringify(data, null, 2), function (err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(
+				"The data has been scraped and saved successfully! View it at './teams.json'"
+			);
+		});
+		// functions.dbEntry(data); // Uncomment to make database entry
+	},
 };
 
 module.exports = scraperObject;
